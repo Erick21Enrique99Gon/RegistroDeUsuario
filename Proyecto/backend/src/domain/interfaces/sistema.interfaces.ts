935e5012bd085121e27a84b377eaf2f8ad5a2790
Erick21Enrique99Gon <61717279+Erick21Enrique99Gon@users.noreply.github.com>
2025-12-20 18:42:09 -0600

    feat:interfaces

---
+++ Proyecto/backend/src/domain/interfaces/sistema.interfaces.ts
@@ -0,0 +1,25 @@
+export interface usuario{
+    id: string;
+    correo_electronico:string;
+    telefono:string;
+    nombres:string;
+    apellidos:string;
+    sexo:string;
+    genero:string;
+    pais:number;
+    contrasenia:string;
+    administrador:boolean;
+    ciudadano:boolean;
+    habilitado:boolean;
+}
+
+export interface pasaporte{
+    id_usuario:string;
+    tipo_de_pasaporte:string;
+    fecha_de_emision:Date;
+    fecha_de_vencimiento:Date;
+    lugar:string;
+    pais_de_emision:number;
+    numero_de_pasaporte:string;
+    habilitado:boolean;
+}
