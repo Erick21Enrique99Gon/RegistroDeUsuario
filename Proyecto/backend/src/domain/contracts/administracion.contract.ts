91d92532112fa5da5d863a26b527fb273bd9bbfe
Erick21Enrique99Gon <61717279+Erick21Enrique99Gon@users.noreply.github.com>
2025-12-20 22:16:37 -0600

    feat:contract

--- Proyecto/backend/src/domain/contracts/administracion.contract.ts
+++ Proyecto/backend/src/domain/contracts/administracion.contract.ts
@@ -2,4 +2,16 @@ import { Usuario, Pasaporte } from "../interfaces/sistema.interfaces";
 
 export abstract class AdministracionContract{
     public abstract registrarUsuario(usuario:Usuario):Promise<void>;
+    public abstract obtenerUsuario(id:string):Promise<Usuario>;
+    public abstract modificarUsuario(usuario:Usuario):Promise<void>;
+    public abstract deshabilitarUsuario(id:string):Promise<void>;
+    public abstract habilitarUsuario(id:string):Promise<void>;
+    public abstract autenticarUsuario(contrasenia:string):Promise<void>;
+    public abstract listarUsuario():Promise<Usuario[]>;
+    public abstract registrarPasaporte(pasaporte:Pasaporte):Promise<void>;
+    public abstract obtenerPasaporte(id_usuario:string,numero_de_pasaporte:string):Promise<Pasaporte>
+    public abstract habilitarPasaporteUsuario(id_usuario:string,numero_de_pasaporte:string):Promise<void>;
+    public abstract deshabilitarPasaporteUsuario(id_usuario:string,numero_de_pasaporte:string):Promise<void>;
+    public abstract listarPasaportes():Promise<Pasaporte[]>
+    public abstract listarPasaportesUsuario(id_usuario:string):Promise<Pasaporte[]>
 }
