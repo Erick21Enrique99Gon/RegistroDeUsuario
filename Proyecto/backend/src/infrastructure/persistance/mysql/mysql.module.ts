b4a0dd6afd789014b4a079a328d765056a11e0cc
Erick21Enrique99Gon <61717279+Erick21Enrique99Gon@users.noreply.github.com>
2025-12-20 23:42:19 -0600

    feat:conexion

---
+++ Proyecto/backend/src/infrastructure/persistance/mysql/mysql.module.ts
@@ -0,0 +1,22 @@
+import mysql from 'mysql2/promise';
+import { Module } from "@nestjs/common";
+
+@Module({
+    providers: [
+        {
+            provide: "MYSQL_CLIENT",
+            useFactory: () =>
+                mysql.createPool({
+                host: process.env.DB_HOST || 'localhost',
+                user: process.env.DB_USER || 'root',
+                password: process.env.DB_PASSWORD || '',
+                database: process.env.DB_NAME || 'ProyectoDB',
+                waitForConnections: true,
+                connectionLimit: 10,
+                queueLimit: 0
+                })
+        },
+    ],
+    exports: ["MYSQL_CLIENT"],
+})
+export class RedisModule {}
